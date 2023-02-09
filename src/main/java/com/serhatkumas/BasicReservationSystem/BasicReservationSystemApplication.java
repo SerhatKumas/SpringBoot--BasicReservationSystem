57f6e4ce39318831dad8520ab8d172ff4286ab0d
Serhat Kumas <serhatkumas@outlook.com>
2023-02-09 21:46:02 +0300

    Project Initialization

---
+++ src/main/java/com/serhatkumas/BasicReservationSystem/BasicReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.serhatkumas.BasicReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serhatkumas/BasicReservationSystem/BasicReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhatkumas.BasicReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
