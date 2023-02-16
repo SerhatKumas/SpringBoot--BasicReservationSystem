2499d76c58b81e0a3f81503aa854d806cf1259d7
Serhat Kumas <serhatkumas@outlook.com>
2023-02-16 23:43:01 +0300

    Reservation model is created

---
+++ src/main/java/Model/Reservation.java
@@ -0,0 +1,22 @@
+package Model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Reservation {
+
+long reservation_id;
+
+Date reservation_date;
+
+Time reservation_time;
+
+String customer_name;
+
+String customer_phone_number;
+
+int customer_age;
+
+String reservation_code;
+
+}
