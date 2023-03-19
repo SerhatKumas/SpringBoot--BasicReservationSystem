cc59c6cdc3e4cb916692a083877c6f45286c979b
Serhat Kumas <serhatkumas@outlook.com>
2023-03-20 01:10:20 +0300

    End points user type is commented for futher auth implementations

--- src/main/java/com/serhatkumas/BasicReservationSystem/ApiLayer/ReservationController.java
+++ src/main/java/com/serhatkumas/BasicReservationSystem/ApiLayer/ReservationController.java
@@ -27,31 +27,31 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    //Welcome message for -> url/reservation-api  endpoint
+    //Welcome message for -> url/reservation-api -> Customer endpoint
     @GetMapping
     public String showWelcomeMessage() {
         return "Welcome to Reservation Api, enter url to reach end point you want";
     }
 
-    //Getting all reservations for -> url/show-all-reservations  endpoint
+    //Getting all reservations for -> url/show-all-reservations -> Admin endpoint
     @GetMapping(path = "show-all-reservations")
     public List<Reservation> getAllReservations() {
         return reservationService.getAllReservations();
     }
 
-    //Getting all future reservation from current date for -> url/show-future-reservations  endpoint
+    //Getting all future reservation from current date for -> url/show-future-reservations -> Admin endpoint
     @GetMapping(path = "show-future-reservations")
     public List<Reservation> getFutureReservations() {
         return reservationService.getFutureReservations();
     }
 
-    //Getting all past reservation from current date for -> url/show-past-reservations  endpoint
+    //Getting all past reservation from current date for -> url/show-past-reservations -> Admin endpoint
     @GetMapping(path = "show-past-reservations")
     public List<Reservation> getPastReservations() {
         return reservationService.getPastReservations();
     }
 
-    //Getting all reservation between 2 given dates for -> url/show-reservations-between  endpoint
+    //Getting all reservation between 2 given dates for -> url/show-reservations-between -> Admin endpoint
     @GetMapping(path = "show-reservations-between")
     public List<Reservation> getReservationsBetweenDates(
             @RequestBody String query_info) {
@@ -70,7 +70,7 @@ public class ReservationController {
         return reservationService.getReservationsBetweenDates(from, until);
     }
 
-    //Getting reservation on a certain date for -> url/show-reservations-date  endpoint
+    //Getting reservation on a certain date for -> url/show-reservations-date -> Admin endpoint
     @GetMapping(path = "show-reservations-date")
     public List<Reservation> getReservationsByDate(@RequestBody String query_info) {
         JSONObject json;
@@ -86,7 +86,7 @@ public class ReservationController {
         return reservationService.getReservationsByDate(date);
     }
 
-    //Getting all reservation of a customer by customer name for -> url/show-all-reservations-by-name  endpoint
+    //Getting all reservation of a customer by customer name for -> url/show-all-reservations-by-name -> Admin endpoint
     @GetMapping(path = "/show-all-reservations-by-name")
     public List<Reservation> getAllReservationOfCustomerByName(@RequestBody String query_info) {
         JSONObject json;
@@ -102,19 +102,19 @@ public class ReservationController {
         return reservationService.getAllReservationOfCustomerByName(customer_name);
     }
 
-    //Getting all reservation on a current date for -> url/show-reservations-of-today  endpoint
+    //Getting all reservation on a current date for -> url/show-reservations-of-today -> Admin endpoint
     @GetMapping(path = "show-reservations-of-today")
     public List<Reservation> getReservationsOfToday() {
         return reservationService.getReservationsOfToday();
     }
 
-    //Getting rest of the reservations on current date with the time parameter for -> url/show-remaining-reservations-today  endpoint
+    //Getting rest of the reservations on current date with the time parameter for -> url/show-remaining-reservations-today -> Admin endpoint
     @GetMapping(path = "show-remaining-reservations-today")
     public List<Reservation> getRestOfTheReservationsToday() {
         return reservationService.getRestOfTheReservationsToday();
     }
 
-    //Getting reservation details by customer name and reservation code for -> url/show-reservations-by-name-and-reservation-code  endpoint
+    //Getting reservation details by customer name and reservation code for -> url/show-reservations-by-name-and-reservation-code -> Customer endpoint
     @GetMapping(path = "/show-reservations-by-name-and-reservation-code")
     public Optional<Reservation> getReservationByReservationCodeAndCustomerName(@RequestBody String query_info) {
         JSONObject json;
@@ -132,8 +132,8 @@ public class ReservationController {
         return reservationService.getReservationByReservationCodeAndCustomerName(customer_name, reservation_code);
     }
 
-    //Getting all not-expired reservations of a customer for -> url/show-active-reservations-by-name  endpoint
-    @GetMapping(path = "/show-active-reservations-by-name")
+    //Getting all not-expired reservations of a customer for -> url/show-active-reservations-by-name -> Admin endpoint
+    @GetMapping(path = "/show-active-reservation-by-name")
     public Optional<Reservation> getActiveReservationOfCustomerByName(@RequestBody String query_info) {
         JSONObject json;
         String customer_name;
@@ -148,8 +148,8 @@ public class ReservationController {
         return reservationService.getActiveReservationOfCustomerByName(customer_name);
     }
 
-    //Getting all not-expired reservations of a customer after certain date for -> url/show-active-reservations-after-certain-date-by-name-date  endpoint
-    @GetMapping(path = "/show-active-reservations-after-certain-date-by-name-date")
+    //Getting all not-expired reservations of a customer after certain date for -> url/show-active-reservations-after-certain-date-by-name-date -> Admin endpoint
+    @GetMapping(path = "/show-active-reservation-after-certain-date-by-name-date")
     public Reservation getActiveReservationOfCustomerAfterCertainDateByNameAndDate(@RequestBody String query_info) {
         JSONObject json;
         String customer_name;
@@ -166,7 +166,7 @@ public class ReservationController {
         return reservationService.getActiveReservationOfCustomerAfterCertainDateByNameAndDate(customer_name, date);
     }
 
-    //Getting a certain reservation on given date and time for -> url/show-reservation-date-time  endpoint
+    //Getting a certain reservation on given date and time for -> url/show-reservation-date-time -> Admin endpoint
     @GetMapping(path = "show-reservation-date-time")
     public Optional<Reservation> getReservationByDateAndTime(@RequestBody String query_info) {
         JSONObject json;
@@ -184,13 +184,13 @@ public class ReservationController {
         return reservationService.getReservationByDateAndTime(date, time);
     }
 
-    //Adding a new reservation for -> url/create-reservation  endpoint
+    //Adding a new reservation for -> url/create-reservation -> Customer endpoint
     @PostMapping(path = "create-reservation")
     public void addNewReservation(@RequestBody Reservation reservation) {
         reservationService.addNewReservation(reservation);
     }
 
-    //Deleting a reservation for -> url/delete-reservation  endpoint
+    //Deleting a reservation for -> url/delete-reservation -> Customer endpoint
     @DeleteMapping(path = "delete-reservation")
     public void deleteReservation(@RequestBody String query_info) {
         JSONObject json;
@@ -206,7 +206,7 @@ public class ReservationController {
         reservationService.deleteReservation(reservation_code);
     }
 
-    //Updating a reservation for -> url/update-reservation  endpoint
+    //Updating a reservation for -> url/update-reservation -> Customer endpoint
     @PutMapping(path = "update-reservation")
     public void updateReservation(
             @RequestBody String query_info) {
@@ -228,7 +228,7 @@ public class ReservationController {
         reservationService.updateReservation(reservation_code, date, time);
     }
 
-    //Getting number of all reservations on the certain date for -> url/how-many-customers-date  endpoint
+    //Getting number of all reservations on the certain date for -> url/how-many-customers-date -> Admin endpoint
     @GetMapping(path = "how-many-customers-date")
     public int getHowManyCustomersOnCertainDate(@RequestBody String query_info) {
         JSONObject json;
@@ -244,25 +244,25 @@ public class ReservationController {
         return reservationService.getHowManyCustomersOnCertainDate(date);
     }
 
-    //Getting number of all reservations on the current date for -> url/how-many-customers-today  endpoint
+    //Getting number of all reservations on the current date for -> url/how-many-customers-today -> Admin endpoint
     @GetMapping(path = "how-many-customers-today")
     public int getHowManyCustomersOnCertainDate() {
         return reservationService.getHowManyCustomersToday();
     }
 
-    //Getting number of all reservations on rest of the certain date with time parameter for -> url/how-many-remaining-customers-today  endpoint
+    //Getting number of all reservations on rest of the certain date with time parameter for -> url/how-many-remaining-customers-today -> Admin endpoint
     @GetMapping(path = "how-many-remaining-customers-today")
     public int getHowManyRemainingCustomersRestOfToday() {
         return reservationService.getHowManyRemainingCustomersRestOfToday();
     }
 
-    //Getting number of customer from the beginning (Not The number of visitation, number of different customers) for -> url/show-total-number-customer  endpoint
+    //Getting number of customer from the beginning (Not The number of visitation, number of different customers) for -> url/show-total-number-customer -> Admin endpoint
     @GetMapping(path = "show-total-number-customer")
     public int getNumberOfTotalCustomer() {
         return reservationService.getNumberOfTotalCustomer();
     }
 
-    //Getting nth best customer according to number of visit they have done for -> url/show-nth-best-customer  endpoint
+    //Getting nth best customer according to number of visit they have done for -> url/show-nth-best-customer -> Admin endpoint
     @GetMapping(path = "show-nth-best-customer")
     public String getBestCustomerByRank(@RequestBody String query_info) {
         JSONObject json;
